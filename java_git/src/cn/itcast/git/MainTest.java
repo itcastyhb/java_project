e014f8540432027b26af4614050a866a85c94146
itcastyhb <yuhongbo@itcast.cn>
2018-12-18 17:16:05 +0800

    项目第2次提交

--- java_git/src/cn/itcast/git/MainTest.java
+++ java_git/src/cn/itcast/git/MainTest.java
@@ -4,5 +4,9 @@ public class MainTest {
 
     public static void main(String[] args) {
         System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
+        System.out.println(11111);
     }
 }
